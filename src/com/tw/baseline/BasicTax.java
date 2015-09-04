4367614c9a82a82c564f0135a76983cb9d74d731
Monica Singh <monicas138@gmail.com>
2015-09-04 16:39:15 +0530

    refactored by adding parameterized constructor for basic tax

--- src/com/tw/baseline/BasicTax.java
+++ src/com/tw/baseline/BasicTax.java
@@ -3,6 +3,13 @@ package com.tw.baseline;
 
 public class BasicTax {
 
+    private String itemName;
+    private double itemPrice;
+
+    public BasicTax(String itemName, double itemPrice) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
     public double calculateBasicTax() {
         return 0.0;
     }
--- test/com/tw/baseline/BasicTaxTest.java
+++ test/com/tw/baseline/BasicTaxTest.java
@@ -9,7 +9,7 @@ public class BasicTaxTest {
 
     @Test
     public void shouldReturnZeroIfNoItemIsPurchased() {
-        BasicTax basicTax = new BasicTax();
+        BasicTax basicTax = new BasicTax(" ", 0.0);
 
         assertEquals(0.0, basicTax.calculateBasicTax(), 0.0d);
     }
